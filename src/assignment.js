f21e10caa64d8030218a75bfcf85ddffcbb485ec
samuel <samuel.ogundare@cloud.coursera.org>
2022-07-15 21:05:10 +0100

    Update implementation

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let result = 0;
+    for(let i=0; i < arrayOfNumbers.length;i++){
+        result += arrayOfNumbers[i];
+    }
+    return result;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,7 +32,13 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let count = 0;
+    for(let i = 0;i<arrayOfNumbers.length;i++){
+        if(arrayOfNumbers[i]%2==0){
+            count++;
+        }
+    }
+    return count;
 }
 // assignment.countEvenNumbers = countEvenNumbers;
 
